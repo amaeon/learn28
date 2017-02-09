1dbb6a24b606ce6fcdf7baa82fcb3fcb5af58632
amaeon <amaeon@gmail.com>
2017-02-09 22:30:09 +0700

    Albedo version, input string must be in right form. Some hacks inside.

--- truthtable.c
+++ truthtable.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <time.h>
+#include "aid.h"
 
 #define MAXLINE 1000
 #define MAXVARS 26
@@ -7,10 +10,20 @@ struct
 {
 	char infix[MAXLINE];
 	char postfix[MAXLINE];
-	char vars[MAXVARS];
-	short order[MAXVARS];
+	char vnum[MAXVARS];		// stores on which place stands i-th var (if stands >=0)
+	char letter[MAXVARS];	// stores which letter stands in i-th place in table sets
+	char nvars;
 }
 typedef Expression;
+
+#define ORDER_ALPHA 0 // alphabetic order
+#define ORDER_EXPR 1  // vars go as they appear in expression
+#define ORDER_RAND 2  // random order
+void initExpression(Expression *expr);
+int infixToPostfix(Expression *expr);
+void setOrder(Expression *expr, int order_type);
+void makeTableTXT(Expression expr);
+char eval(Expression expr, char *set);
 
 // vars for stack
 #define MAXSTACK 200
@@ -19,50 +32,80 @@ char *sp = stk;
 // methods for stack
 char pop();
 void push(char c);
-void printStk();
-int emptyStk();
+void printStk();	// has been used for debug
+int stackEmpty();
 int topOp();
+void resetStack();
 
-void initExpression(Expression *expr);
-int fgetline(char *dest, int lim);
+int fgetline(FILE *f, char *dest, int lim);
 
 // all for operations
 #define EMPTY ' '
-#define NEG '~'
+#define NEG '!'
+#define NEG2 '~'
 #define AND '*'
 #define OR '+'
 #define XOR '^'
 #define THEN '>'
 #define EQ '='
-int priority(char c);
+int priority(char c);	// returns relative priority of an operation 
+
+// some things for list
+struct node
+{
+	char val;
+	struct node *next;
+}
+typedef Node;
+
+// list made to be with first fictive element, for simplicity
+Node* initList();
+void addNode(Node *head, char c);
+char extractNode(Node *head, int n);
+void printList(Node *head);	// has been used for debug
 
 
-int infixToPostfix(Expression *expr);
-void makeTableTXT();
-char eval();
-
+//======================================//
+//              DEFINITION              //
+//======================================//
 int main(int argc, char const *argv[])
 {
 	Expression e;
 	initExpression(&e);
-	infixToPostfix(&e);
-
-	printf("%s\n",e.postfix);
-
-
+	setOrder(&e,ORDER_RAND);
+	makeTableTXT(e);
+	
 	return 0;
 }
 
 void initExpression(Expression *expr)
 {
-	fgetline(expr->infix,MAXLINE);
-}
-
-int fgetline(char *dest, int lim)
-{
+	// read line from file
 	FILE *f;
 	f = fopen("input.txt","r");
+	if (f==NULL){
+		fprintf(stderr, "No input file! Expression must be in input.txt\nDefault expression used\n");
+		*(expr->infix)=NEG;
+		*(expr->infix+1)='A';
+		*(expr->infix+2)='\0';
+	}
+	else{
+		fgetline(f,expr->infix,MAXLINE);
+		fclose(f);
+	}
+	*(expr->postfix)='\0';
+	// impossimble numbers for each var
+	for(int i=0; i<MAXVARS; i++){
+		expr->vnum[i]=-1;
+		expr->letter[i]=-1;
+	}
+	expr->nvars=0;
+	setOrder(expr,ORDER_ALPHA);	// alphabetic order by default, can be reset later
+	infixToPostfix(expr);
+}
 
+int fgetline(FILE *f, char *dest, int lim)
+{
 	int i;
 	char *tp = dest;
 	char c;
@@ -71,18 +114,7 @@ int fgetline(char *dest, int lim)
 		tp++;
 	}
 	*tp='\0';
-
-	fclose(f);
 	return i;
-}
-
-void makeTableTXT(char *pfx)
-{
-	FILE *f;
-	f = fopen("table.txt","w");
-
-
-	fclose(f);
 }
 
 int infixToPostfix(Expression *expr)
@@ -94,13 +126,12 @@ int infixToPostfix(Expression *expr)
 
 	// Implementation of the Shunting-yard algotithm
 	while((op1=*tp)!='\0'){
-		//printf("\nINSTRING %c",*tp);
 		if(isalpha(op1)){
 			*mp=toupper(op1);
 			mp++;
 		}
 		else switch(op1){
-			case NEG:{
+			case NEG: case NEG2:{
 				push(op1);
 				break;
 			}
@@ -154,11 +185,11 @@ int infixToPostfix(Expression *expr)
 				break;
 			}
 			case ')':{
-				while( !emptyStk() && (op2=pop())!='(' ){
+				while( !stackEmpty() && (op2=pop())!='(' ){
 					*mp = op2;
 					mp++;
 				}
-				if(emptyStk() && op2!='('){
+				if(stackEmpty() && op2!='('){
 					fprintf(stderr,"Incorrect sequence: some brackets missing!\n");
 					return -1;
 				}
@@ -172,7 +203,8 @@ int infixToPostfix(Expression *expr)
 		}
 		tp++;
 	}
-	while(!emptyStk()){
+	// pop all remaining from stack
+	while(!stackEmpty()){
 		if (topOp()){
 			*mp = pop();
 			mp++;
@@ -183,34 +215,209 @@ int infixToPostfix(Expression *expr)
 			break;
 		}
 	}
+	// place line ending for prefix form
 	*mp='\0';
 	return 0;
 }
 
+void setOrder(Expression *expr, int order_type)
+{
+	for(int i=0; i<MAXVARS; i++){
+		expr->vnum[i]=-1;
+		expr->letter[i]=-1;
+	}
+	char *p = expr->infix;
+	char var;
+	char *vnum = expr->vnum;
+	char *letter = expr->letter;
+
+	switch(order_type){
+		case ORDER_ALPHA:{
+			while(*p!='\0'){
+				if (isalpha(*p)){
+					var = toupper(*p)-'A';
+					if(vnum[var]<0)
+						vnum[var]=0;
+				}
+				p++;
+			}
+			int n=0;
+			for(int i=0; i<MAXVARS; i++)
+				if(vnum[i]==0){
+					vnum[i]=n;
+					letter[n]=i;
+					n++;
+				}
+			expr->nvars=n;
+			break;
+		}
+		case ORDER_EXPR:{
+			int n=0;
+			while(*p!='\0'){
+				if(isalpha(*p)){
+					var = toupper(*p)-'A';
+					if(vnum[var]<0){
+						vnum[var]=n;
+						n++;
+					}
+				}
+				p++;
+			}
+			for(int i=0; i<MAXVARS; i++)
+				if(vnum[i]>=0)
+					letter[vnum[i]]=i;
+			expr->nvars=n;
+			break;
+		}
+		case ORDER_RAND:{
+			int n=0;
+			while(*p!='\0'){
+				if(isalpha(*p)){
+					var = toupper(*p)-'A';
+					if(vnum[var]<0){
+						vnum[var]=0;
+						n++;
+					}
+				}
+				p++;
+			}
+			expr->nvars=n;
+			Node *numlist = initList();
+			for(int i=0; i<n; i++)
+				addNode(numlist,i);
+			srand(time(NULL));
+			for(int i=0; i<MAXVARS; i++)
+				if(vnum[i]==0){
+					vnum[i]=extractNode(numlist,rand()%n);
+					letter[vnum[i]]=i;
+					n--;
+				}
+			break;
+		}
+		default:{	// alphabetic order by default
+			setOrder(expr,ORDER_ALPHA);
+			return;
+		}
+	}
+}
+
+void makeTableTXT(Expression expr)
+{
+	FILE *f;
+	f = fopen("table.txt","w");
+
+	for(int i=0; i<expr.nvars; i++)
+		fprintf(f,"%c",expr.letter[i]+'A');
+	fprintf(f," %s\n",expr.infix);
+	for(int i=0; i<expr.nvars +1 +10; i++)
+		fprintf(f, "-");
+	fprintf(f, "\n");
+
+	int nlines = ipow(2,expr.nvars);
+	char set[MAXVARS];
+	int t;
+	for(int i=0; i<nlines; i++){
+		t=i;
+		for(int j=expr.nvars-1; j>=0; j--){
+			set[j]=t%2;
+			t>>=1;
+		}
+		for(int j=0; j<expr.nvars; j++)
+			fprintf(f,"%c",set[j]+'0');
+		fprintf(f," %c\n",eval(expr,set)+'0');
+	}
+
+	fclose(f);
+}
+
+char eval(Expression expr, char *set)
+{
+	resetStack();
+	char *line = expr.postfix;
+	char *vnum = expr.vnum;
+	char op2;
+	while(line-(expr.postfix)+1<MAXLINE && *line!='\0'){
+		switch(*line){
+			case NEG: case NEG2:{
+				if(pop()==1)
+					push(0);
+				else
+					push(1);
+				break;
+			}
+			case AND:{
+				op2=pop();
+				if(pop()==1 && op2==1)
+					push(1);
+				else
+					push(0);
+				break;
+			}
+			case OR:{
+				op2=pop();
+				if(pop()==0 && op2==0)
+					push(0);
+				else
+					push(1);
+				break;
+			}
+			case XOR:{
+				op2=pop();
+				if(pop()==op2)
+					push(0);
+				else
+					push(1);
+				break;
+			}
+			case THEN:{
+				op2=pop();
+				if(pop()==1 && op2==0)
+					push(0);
+				else
+					push(1);
+				break;
+			}
+			case EQ:{
+				op2=pop();
+				if(pop()==op2)
+					push(1);
+				else
+					push(0);
+				break;
+			}
+			default:{
+				if(isalpha(*line))
+					push(set[vnum[*line-'A']]);
+				break;
+			}
+		}
+		line++;
+	}
+	if (!stackEmpty())
+		return pop();
+	else
+		return -1;
+}
+
 char pop()
 {
-	//printf("\nPOP %c",*(sp-1));
 	if(sp!=stk){
 		sp--;
-		//printStk(); // -
 		return *(sp);
 	}
 	else{
 		fprintf(stderr, "Stack is empty! Can't pop!\n" );
-		//printStk(); // -
 		return 0;
 	}
 }
 
 void push(char c){
-	//printf("\nPUSH %c",c);
 	if(sp-stk+1<MAXSTACK){
 		*sp=c;
 		sp++;
 	}
 	else
 		fprintf(stderr, "Stack is full! Can't push!\n" );
-	//printStk();
 }
 
 void printStk()
@@ -223,7 +430,7 @@ void printStk()
 	}
 }
 
-int emptyStk()
+int stackEmpty()
 {
 	return !(sp>stk);
 }
@@ -232,7 +439,7 @@ int priority(char c)
 {
 	switch(c){
 		case EMPTY:{return 999; break;}
-		case NEG:{return 5; break;}
+		case NEG: case NEG2:{return 5; break;}
 		case AND:{return 4; break;}
 		case OR:{return 3; break;}
 		case XOR:{return 3; break;}
@@ -245,10 +452,61 @@ int priority(char c)
 
 int topOp()
 {
-	if(emptyStk())
+	if(stackEmpty())
 		return 0;
 	if(*(sp-1)=='(')
 		return 0;
 	else
 		return 1;
+}
+
+void resetStack()
+{
+	sp = stk;
+}
+
+Node* initList()
+{
+	Node *p;
+	p = (Node*)malloc(sizeof(Node));
+	p->next=NULL;
+	p->val=-1;
+}
+
+void addNode(Node *head, char c)
+{
+	Node *p = head;
+	while(p->next!=NULL)
+		p=p->next;
+	p->next= (Node*)malloc(sizeof(Node));
+	p=p->next;
+	p->next=NULL;
+	p->val=c;
+}
+
+char extractNode(Node *head, int n)
+{
+	Node *p = head;
+	int i;
+	for(i=n; i>0 && p->next!=NULL; i--)
+		p=p->next;
+	if(i!=0 || head->next==NULL){	// list ended
+		fprintf(stderr,"\nElement #%d not found in list!\n",n);
+		return -1; // possible char with value = -1 here?
+	}
+	Node *nxt = p->next->next;
+	char val=p->next->val;
+	free(p->next);
+	p->next = nxt;
+	return val;
+}
+
+void printList(Node *head)
+{
+	Node *p = head->next;
+	while(p!=NULL){
+		printf("%d ",p->val);
+		p=p->next;
+	}
+	printf("\n");
 }
